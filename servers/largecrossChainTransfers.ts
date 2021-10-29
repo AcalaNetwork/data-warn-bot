53fd995a037c1010b0222e71b75d378e55a10eb5
倪永胜 <714275326@qq.com>
2021-10-29 18:54:35 +0800

    add-largecrossChainTransfers

---
+++ servers/largecrossChainTransfers.ts
@@ -0,0 +1,40 @@
+import { FixedPointNumber, forceToCurrencyIdName } from "@acala-network/sdk-core";
+import { CurrencyId } from "@acala-network/types/interfaces";
+import { u128, u64 } from "@polkadot/types";
+import { encodeAddress } from "@polkadot/util-crypto";
+import { config } from "../config";
+import { LARGE_XTOKENS_TRANSFER, Logger } from "../utils";
+
+interface XTokensArgs {
+  currency_id: {
+    token: string
+  };
+  amount: u128,
+  dest: {
+    v1: {
+      parents: number,
+      interior: {
+        x1: {
+          accountId32: {
+            network: any,
+            id: string
+          }
+        }
+      }
+    }
+  },
+  dest_weight: u64;
+}
+
+export const largecrossChainTransfers = async (height: number, args: any) => {
+  const { currency_id, amount, dest, dest_weight } = args as XTokensArgs;
+
+  const token = currency_id.token;
+  const total = FixedPointNumber.fromInner(amount.toString(), config.ksm.decimal).toNumber();
+  const account = encodeAddress(dest.v1.interior.x1.accountId32.id.toString(), config.kar.prefix);
+  const parents = dest.v1.parents;
+
+  if(total > 100 && token === 'KSM') {
+    Logger.pushEvent(LARGE_XTOKENS_TRANSFER, `%%% \n - block_height: __${height}__ \n - sender: __${account}__ \n - dest: - parents: ${parents} \n - currency_id: __${token}__ \n - amount: __${total}__ \n %%%`);
+  } 
+}
--- servers/subLeastestHeader.ts
+++ servers/subLeastestHeader.ts
@@ -1,5 +1,5 @@
-import { Logger, SCANNER_ERROR } from "../utils/logger";
-import { SubscribeBlock, SubscribeBlockError } from "@open-web3/scanner/types";
+import { Logger } from "../utils/logger";
+import { SubscribeBlock } from "@open-web3/scanner/types";
 
 // lastest block
 let header = 0;
@@ -8,15 +8,11 @@ let warnintTimer: NodeJS.Timeout | null = null;
 // send wrong message to datadog time;
 const timing = 1000 * 60 * 10;
 
-export const subLeastestHeader = async (block: SubscribeBlock | SubscribeBlockError) => {
-  if(block.error) {
-    Logger.pushEvent(SCANNER_ERROR, 'Subscribe Block Error', 'normal', 'warning');
-    return Logger.error('Subscribe Block Error')
-  }
+export const subLeastestHeader = async (block: SubscribeBlock) => {
   header = block.blockNumber;
   Logger.log('Get new block: ' + header);
   warnintTimer && clearInterval(warnintTimer)
   warnintTimer = setInterval(() => {
-    Logger.error('Get node height timeout');
+    Logger.error('Get chain block height timeout');
   }, timing);
 }
