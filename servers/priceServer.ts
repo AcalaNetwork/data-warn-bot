c769c1955a62a84cd9e8693bb46db8ef074f9c87
shenger <714275326@qq.com>
2022-07-20 15:32:58 +0800

    fix

--- servers/priceServer.ts
+++ servers/priceServer.ts
@@ -4,8 +4,6 @@ import { Logger, PRICE_SERVER } from '../utils';
 export const priceServer = async () => {
   const data = await axios.get('https://api.polkawallet.io/price-server/?token=KSM&from=market');
 
-  console.log(data)
-
   if(data.status === 200) {
     Logger.pushEvent(PRICE_SERVER, '', 'normal', 'info');
   }
